54ecdef7c1d70897a754a62dfb3e1650f5903d9c
orelsgit <orelsmail@gmail.com>
2022-06-27 15:15:06 +0300

    - RDS integration works + queries

--- src/api.js
+++ src/api.js
@@ -34,20 +34,98 @@ const client = new Client({
 
 app.use(express.json());
 
-app.get("/users", async function (req, res) {
+app.get("/users/:id", async function (req, res) {
   try {
-    console.log(process.env.USERNAME)
     client.connect();
-    let users = await client.query(`SELECT * FROM users`)
+    const id = req.params.id;
+    let user = (await client.query(`SELECT * FROM users WHERE id = '${id}'`)).rows;
     res.json({
-      users,
-      executionDate: `${(new Date()).getTime()}`,
+      result: user,
     })
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Could not create user" });
   }
 });
+
+app.post("/users", async function (req, res) {
+  try {
+    client.connect();
+    const { first_name, last_name, gender, email, number } = req.body;
+    const id = v4();
+    let result = (await insert(
+      USERS_TABLE,
+      ['id', 'first_name', 'last_name', 'gender', 'email', 'number'],
+      [id, first_name, last_name, gender, email, number]
+    ));
+    res.json({
+      result,
+      executionDate: ` ${(new Date()).getTime()}`,
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create user" });
+  }
+});
+
+app.post("/folders", async function (req, res) {
+  try {
+    client.connect();
+    const { title, position, user_id } = req.body;
+    const id = v4();
+    let result = (await client.query(`insert into folders(id, title, times_used, position, user_id)
+    values ('${id}', '${title}', 0, ${position ? position : 0}, '${user_id}')`));
+    res.json({
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create folder" });
+  }
+});
+
+app.get("/folders/:user_id", async function (req, res) {
+  try {
+    client.connect();
+    const { user_id } = req.params;
+    let result = (await client.query(`SELECT * FROM ${FODLERS_TABLE} WHERE user_id = ${user_id}`)).rows;
+    res.json({
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create folder" });
+  }
+});
+
+/**
+ * Inserts [values] into [columns] in [table_name], according
+ * to the order of columns and values. ex.: columns[0] = values[0].
+ * @param {string} table_name is the name of the table
+ * @param {[string]} columns are the columns to insert
+ * @param {[string]} values are the values to insert
+ * @returns the row inserted.
+ */
+const insert = async (table_name, columns, values) => {
+  if (!Array.isArray(columns) || !Array.isArray(values)) {
+    throw Error('columns and values must be arrays');
+  }
+  try {
+    let query = `insert into ${table_name}(`;
+    columns.forEach((column) => query += `${column}, `);
+    query = `${query.substring(0, query.length - 2)})\n`;
+    query += `values (`;
+    values.forEach((value) => query += `'${value}', `);
+    query = `${query.substring(0, query.length - 2)})\n`;
+    console.log(query)
+    let result = await client.query(query);
+    console.log(result);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+
+}
 
 app.use((req, res, next) => {
   return res.status(404).json({
